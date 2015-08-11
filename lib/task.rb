bec6d336014705b46506fa4987c8a9c9643d6e27
Travis Crampton & Tim Kellogg <student@epicodus.com>
2015-08-11 10:05:34 -0700

    save tasks accordingly

--- lib/task.rb
+++ lib/task.rb
@@ -1,9 +1,23 @@
 class Task
+
+  @@tasks = []
+
   define_method(:initialize) do |description|
     @description = description
   end
 
   define_method(:description) do
     @description
+  end
+
+  define_singleton_method(:all) do
+    @@tasks
+  end
+
+  define_method(:save) do
+    @@tasks.push(self)
+  end
+  define_singleton_method(:clear) do
+    @@tasks = []
   end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -2,10 +2,36 @@ require('rspec')
 require('task')
 
 describe(Task) do
+
+  before() do
+    Task.clear()
+  end
   describe('#description') do
     it('returns the description of the task') do
       task = Task.new("this is a description")
       expect(task.description()).to(eq("this is a description"))
+    end
+  end
+
+  describe('.all') do
+    it('should be empty by default') do
+      expect(Task.all()).to(eq([]))
+    end
+  end
+  describe('.save') do
+    it('adds task to array of saved tasks') do
+      test_task = Task.new("Do something else")
+      test_task.save()
+      expect(Task.all()).to(eq([test_task]))
+    end
+  end
+
+  describe('.clear') do
+    it('empties out the array of saved tasks') do
+      test_task = Task.new("another thing")
+      test_task.save()
+      Task.clear()
+      expect(Task.all()).to(eq([]))
     end
   end
 end
